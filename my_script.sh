f2f1d43064400f63e2c4d09f19342dde3d2d9239
abcjan88 <39003423+abcjan88@users.noreply.github.com>
2020-09-15 20:36:07 +0700

    Add files via upload

---
+++ my_script.sh
@@ -0,0 +1,7 @@
+pkg  update &​& pkg upgrade -​Y
+pkg  install git -​y
+pkg  install python -​y
+pkg  install python2 -​Y
+git  clone https://github.com/Inject-ID/tombol-special-termux  
+cd  tombol-special-termux  
+python2  special.py  
